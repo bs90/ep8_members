093f3e07a77b563cc70d58708175b9c0f50e8dc1
luongdm-3080 <105909649+luongdm-3080@users.noreply.github.com>
2024-05-28 14:52:04 +0900

    Revert "create model progoses"

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_27_052921) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_042107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "progoses", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.date "date_test", null: false
-    t.integer "range", null: false
-    t.text "range_evaluation"
-    t.integer "accuracy", null: false
-    t.text "accuracy_evaluation"
-    t.integer "fluency", null: false
-    t.text "fluency_evaluation"
-    t.integer "interaction", null: false
-    t.text "interaction_evaluation"
-    t.integer "coherence", null: false
-    t.text "coherence_evaluation"
-    t.integer "phonology", null: false
-    t.text "phonology_evaluation"
-    t.integer "overall", null: false
-    t.text "overall_evaluation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_progoses_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name_roma", limit: 30
@@ -50,5 +28,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_27_052921) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "progoses", "users"
 end
